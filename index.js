6ed8bf2c2cbaabd7614b04da1060c46803589988
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 21:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class BoardMember{
   sayHi = function () {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
-
+}
 
 class CEO extends BoardMember {
   hireEmployee() {
