dd428f7237e30dbaaafbd739509f90868d5c649f
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 21:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
- BoardMember.prototype.veto = function() {
+  veto = function() {
   return "No, I must disagree"
 }
- BoardMember.prototype.approve = function() {
+ approve = function() {
   return "You can do that!"
 }
- BoardMember.prototype.doCharity = function() {
+ doCharity = function() {
   return "I like to help people."
 }
- BoardMember.prototype.releasePressStatement = function() {
+ releasePressStatement = function() {
   return "You will see great things from Scuber."
 }
- BoardMember.prototype.sayHi = function () {
+  sayHi = function () {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
-class CEO extends BoardMember (name, training, homestate){
-
+class CEO extends BoardMember {
+  hireEmployee() {
+    return 'Welcome aboard!'
+  }
 }
